092a345cdaa69856dea763726328214269db478c
navedmohammad <36394261+navedmohammad@users.noreply.github.com>
2024-05-26 18:07:56 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-api_id = 29410389
-api_hash = "0c716764715886f6641477ffbb63e1ee"
-bot_token = "5946934925:AAGoALZ3-Cr8hQaBdzHhmW3pknt-h1LVVpI"
+api_id = 20483216
+api_hash = "2518170d3dd939b3f2893cb0aae805c4"
+bot_token = "7054832669:AAGDS-LhvS14dy3yejqMfEJOVl3arknQAps"
 sudo_groups = -1001777454744
 
